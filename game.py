ad73120690ded059e23a35b48c1bfb8ac1255d12
Noceo200 <onoel2050@gmail.com>
2022-04-27 13:32:49 +0200

    - Implémentation des 6 boutons d'informations - Implémentation d'une partie de la fonction pop-up

--- game.py
+++ game.py
@@ -55,7 +55,7 @@ wagon_pile = Draw_pile(wagon_cards,(0.783, 0.405),0.24)
 
 #Création des autres boutons intéractifs
 
-
+#boutons qui affichent le nombre de carte wagons de chaque couleur que possède l'utilisateur
 wagon_bleu_button = Button((0.23, 0.79),scale = 0.07,center=True,texte = "0")
 wagon_rose_button = Button((0.27, 0.79),scale = 0.07,center=True,texte = "0")
 wagon_rouge_button = Button((0.31, 0.79),scale = 0.07,center=True,texte = "0")
@@ -66,6 +66,7 @@ wagon_vert_button = Button((0.47, 0.79),scale = 0.07,center=True,texte = "0")
 wagon_blanc_button = Button((0.51, 0.79),scale = 0.07,center=True,texte = "0")
 wagon_tout_button = Button((0.55, 0.79),scale = 0.07,center=True,texte = "0")
 
+#bouton qui affichent des informations lors du passage de la souris dessus
 info1_button = Button((0.14, 0.93),scale = 0.07,center=True,image = 'Resources\info_button.png',image2='Resources\info_pioche.png')
 info2_button = Button((0.19, 0.75),scale = 0.07,center=True,image = 'Resources\info_button.png',image2='Resources\info_pioche.png')
 info3_button = Button((0.78, 0.75),scale = 0.07,center=True,image = 'Resources\info_button.png',image2='Resources\info_pioche.png')
@@ -73,11 +74,16 @@ info4_button = Button((0.97, 0.92),scale = 0.07,center=True,image = 'Resources\i
 info5_button = Button((0.97, 0.04),scale = 0.07,center=True,image = 'Resources\info_button.png',image2='Resources\info_pioche.png')
 info6_button = Button((0.72, 0.04),scale = 0.07,center=True,image = 'Resources\info_button.png',image2='Resources\info_pioche.png')
 
+#bouton qui affichent les nombres de crédits et de wagons restant de l'utilisateur et de l'IA
+credit_txt = Button((0.91, 0.91),scale = 0.09,center=True,texte = "1")
+wagon_txt = Button((0.09, 0.91),scale = 0.09,center=True,texte = "43")
+credit_txt_IA = Button((0.63, 0.045),scale = 0.09,center=True,texte = "0")
+wagon_txt_IA = Button((0.40, 0.045),scale = 0.09,center=True,texte = "35")
 
 #ordre à respecter pour la liste : "rose","blanc","bleu","jaune","orange","noir","rouge","vert","tout"...
 buttons = [wagon_rose_button,wagon_blanc_button,wagon_bleu_button,wagon_jaune_button,wagon_orange_button,
            wagon_noir_button,wagon_rouge_button,wagon_vert_button,wagon_tout_button,info1_button,
-           info2_button,info3_button,info4_button,info5_button,info6_button]
+           info2_button,info3_button,info4_button,info5_button,info6_button,credit_txt,wagon_txt,credit_txt_IA,wagon_txt_IA]
 
 #Création et affichage du plateau
 
